952b37bdc78a4cd11642a86ab94b9dac0a156185
Abdullah7758757u46 <46163642+Abdullah7758757u46@users.noreply.github.com>
2018-12-29 00:56:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("528319778213986304")
 setInterval(function() {
 channel.send('هلا');
-}, 30)
+}, 10)
 })
 
 client.login('NTI3NjU3NDY2NTc4NzMxMDA4.DwgWvw.T9cnCrO1gWf9A3q-WJZtOI1OPKI');
