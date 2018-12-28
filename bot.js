ddf7adaa47fb11403c429bacefdedfdc385c5d81
Abdullah7758757u46 <46163642+Abdullah7758757u46@users.noreply.github.com>
2018-12-29 00:25:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
-
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("528319778213986304")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(هلا);
 }, 30)
 })
 
-client.login(process.env.BOT_TOKEN);
+client.login('528307636492435456.JAL8WS5QGxP6LCKAm-JveBew-P4');
