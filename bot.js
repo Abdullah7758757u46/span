453ffc54b6a5685c64ddd6ab80d52515efd8f9e3
Abdullah7758757u46 <46163642+Abdullah7758757u46@users.noreply.github.com>
2018-12-29 05:57:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,8 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("528319778213986304")
 setInterval(function() {
-}, 10 )
+channel.send(`انا جوعان وكلوني`);
+}, 10)
 })
 
 client.login('NTI3NjU3NDY2NTc4NzMxMDA4.DwgWvw.T9cnCrO1gWf9A3q-WJZtOI1OPKI');
