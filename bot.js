d579fbdf21122238e4ec56b4a7f600dc56a9840d
Abdullah7758757u46 <46163642+Abdullah7758757u46@users.noreply.github.com>
2018-12-29 06:01:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,5 +9,17 @@ setInterval(function() {
 channel.send(`انا جوعان وكلوني`);
 }, 10)
 })
+client.on("ready", () => {
+let channel =     client.channels.get("528319778213986304")
+setInterval(function() {
+channel.send(`انا جوعان وكلوني`);
+}, 10)
+})
+client.on("ready", () => {
+let channel =     client.channels.get("528319778213986304")
+setInterval(function() {
+channel.send(`انا جوعان وكلوني`);
+}, 10)
+})
 
 client.login('NTI3NjU3NDY2NTc4NzMxMDA4.DwgWvw.T9cnCrO1gWf9A3q-WJZtOI1OPKI');
